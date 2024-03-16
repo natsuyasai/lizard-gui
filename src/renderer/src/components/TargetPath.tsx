8de954fb24e266b230f46169a104666ff688ed1f
natsuyasai <yasai038tomato@gmail.com>
2024-03-16 21:19:22 +0900

    ファイル選択ベース作成

--- src/renderer/src/components/TargetPath.tsx
+++ src/renderer/src/components/TargetPath.tsx
@@ -1,11 +1,26 @@
-import { FC } from 'react'
-//import styles from "./Target.module.scss";
+import { FC, useState } from 'react'
+import styles from './TargetPath.module.scss'
 
 export const TargetPath: FC = () => {
+  const [path, setPath] = useState('')
+
+  function selectFiles(event: React.ChangeEvent<HTMLInputElement>): void {
+    const files = event.currentTarget.files
+    if (!files || files.length === 0) {
+      return
+    }
+    setPath(files[0].path)
+  }
   return (
     <>
-      {' '}
-      <div>TargetPath</div>{' '}
+      <div className={styles.root}>
+        <div>TargetPath</div>
+        <input readOnly={true} className={styles.input} value={path}></input>
+        <input type="file" id="file-input" onChange={selectFiles}></input>
+        <label htmlFor="file-input" className={styles.label}>
+          Select
+        </label>
+      </div>
     </>
   )
 }
