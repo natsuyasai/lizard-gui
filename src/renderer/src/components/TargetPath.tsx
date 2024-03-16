4082a010a602a2fe5fb52c03f9baec47622d29d9
natsuyasai <yasai038tomato@gmail.com>
2024-03-16 21:40:15 +0900

    フォルダー選択対応

--- src/renderer/src/components/TargetPath.tsx
+++ src/renderer/src/components/TargetPath.tsx
@@ -1,22 +1,36 @@
 import { FC, useState } from 'react'
 import styles from './TargetPath.module.scss'
+import path from 'path'
 
 export const TargetPath: FC = () => {
-  const [path, setPath] = useState('')
+  const [targetPath, setTargetPath] = useState('')
 
   function selectFiles(event: React.ChangeEvent<HTMLInputElement>): void {
     const files = event.currentTarget.files
     if (!files || files.length === 0) {
       return
     }
-    setPath(files[0].path)
+    const file = files[0]
+    const isDir = files.length > 2
+    if (isDir) {
+      const dirpath = path.dirname(file.path)
+      setTargetPath(dirpath)
+    } else {
+      setTargetPath(file.path)
+    }
   }
   return (
     <>
       <div className={styles.root}>
         <div>TargetPath</div>
-        <input readOnly={true} className={styles.input} value={path}></input>
-        <input type="file" id="file-input" onChange={selectFiles}></input>
+        <input readOnly={true} className={styles.input} value={targetPath}></input>
+        <input
+          type="file"
+          directory=""
+          webkitdirectory=""
+          id="file-input"
+          onChange={selectFiles}
+        ></input>
         <label htmlFor="file-input" className={styles.label}>
           Select
         </label>
