9e4c2994835f00b5fd32d28737578f7d15684e77
natsuyasai <yasai038tomato@gmail.com>
2024-03-20 17:53:54 +0900

    validatorのテスト追加

---
+++ src/renderer/src/validator/test/fileNameValidator.spec.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from 'vitest'
+import { FileNameValidator } from '../fileNameValidator'
+
+describe('ファイル名の確認', () => {
+  test.each([
+    ['test', true],
+    [
+      '254aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      true
+    ],
+    [
+      '255aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      false
+    ],
+    [
+      '256aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      false
+    ],
+    ['t\\est', false],
+    ['te/st', false],
+    ['te:st', false],
+    ['te*st', false],
+    ['te?st', false],
+    ['te<st', false],
+    ['te>st', false],
+    ['tes|t', false],
+    ['te"st', false],
+    ['CON', false],
+    ['ACON', true],
+    ['CONA', true],
+    ['COM0', false]
+  ])(
+    '不正なファイル名の場合は失敗となること(filename: %s)',
+    (fileName: string, expected: boolean) => {
+      const target = new FileNameValidator(fileName)
+
+      const result = target.validate()
+      expect(result).toBe(expected)
+    }
+  )
+})
