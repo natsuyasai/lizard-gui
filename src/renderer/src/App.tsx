9e8b49d096a055273f8a94d88eb059ef3bd736ac
natsuyasai <yasai038tomato@gmail.com>
2024-03-20 14:48:36 +0900

    actionボタンつなぎこみ

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -17,13 +17,34 @@ function App(): JSX.Element {
     addParameter: ''
   }
   const [parameter, setParameter] = useState(initParam)
+
+  const [enableExecute, setEnableExecute] = useState(false)
+  const [enableCancel, setEnableCancel] = useState(false)
+
+  function execute() {
+    setEnableCancel(true)
+    setEnableExecute(false)
+  }
+
+  function cancel() {
+    setEnableCancel(false)
+    setEnableExecute(true)
+  }
+
+  function updateTargetPath(targetPath: string) {
+    setParameter({ ...parameter, ...{ targetPath } })
+    const canExecute = targetPath !== ''
+    setEnableExecute(canExecute)
+  }
+
   return (
     <>
       <div className={styles.app_root}>
+        {/* 入力 */}
         <div className={styles.contents}>
           <TargetPath
             targetPath={parameter.targetPath}
-            setTargetPath={(targetPath) => setParameter({ ...parameter, ...{ targetPath } })}
+            setTargetPath={(targetPath) => updateTargetPath(targetPath)}
           ></TargetPath>
           <LanguageSelect
             selectedItem={parameter.language}
@@ -42,10 +63,16 @@ function App(): JSX.Element {
             setParameter={(addParameter) => setParameter({ ...parameter, ...{ addParameter } })}
           ></ParameterInput>
         </div>
+        {/* 実行 */}
         <div className={styles.bottom}>
           <hr className="separator"></hr>
           <div className={styles.action_root}>
-            <Action></Action>
+            <Action
+              enableExecute={enableExecute}
+              enableCancel={enableCancel}
+              onExecute={execute}
+              onCancel={cancel}
+            ></Action>
           </div>
         </div>
       </div>
--- src/renderer/src/components/Action.tsx
+++ src/renderer/src/components/Action.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react'
 import styles from './Action.module.scss'
 
-export const Action: FC = () => {
+interface Props {
+  enableExecute: boolean
+  enableCancel: boolean
+  onExecute: () => void
+  onCancel: () => void
+}
+
+export const Action: FC<Props> = ({ enableExecute, enableCancel, onExecute, onCancel }) => {
   return (
     <>
-      <button className={styles.cahcel}>Cancel</button>
-      <button className={`${styles.exec} execbutton`}>Execute</button>
+      <button className={styles.cahcel} onClick={onCancel} disabled={!enableCancel}>
+        Cancel
+      </button>
+      <button className={`${styles.exec} execbutton`} onClick={onExecute} disabled={!enableExecute}>
+        Execute
+      </button>
     </>
   )
 }
