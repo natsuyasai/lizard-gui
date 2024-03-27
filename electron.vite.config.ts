f294ba74dab0013d4004ecad29c47cc9a8c154b1
natsuyasai <yasai038tomato@gmail.com>
2024-03-27 21:08:31 +0900

    ビルドエラー修正

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,12 +1,24 @@
-import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   main: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@': resolve('src/')
+      }
+    },
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@': resolve('src/')
+      }
+    },
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
